e28df2686506fa83d062344947bd8e30b0403a35
lewo <lewo@abesis.fr>
2017-01-05 00:08:20 +0100

    test

--- configuration.nix
+++ configuration.nix
@@ -1,4 +1,30 @@
+{ config, lib, pkgs, ... }:
+
 {
-  imports = [ <nixpkgs/nixos/modules/virtualisation/nova-image.nix>
+  imports = [ ./hardware.nix
       	      <nixpkgs/nixos/modules/installer/cd-dvd/channel.nix> ];
+
+      services.openssh.enable = true;
+      services.openssh.permitRootLogin = "yes";
+
+      users.users.root.password = "root";
+
+      system.autoUpgrade = {
+        enable = true;
+	channel = "https://nixos.org/channels/nixos-unstable";
+	extraPath = [ pkgs.git ];
+	preRebuild =
+	''
+        mkdir -p /etc/nixos/
+	pushd /etc/nixos/
+	git init
+        git remote add origin https://github.com/nlewo/configuration.git || true
+	git fetch
+        git reset --hard origin/master
+	popd
+	'';
+	dates = null;
+	period = "1m";
+	};
+
 }
