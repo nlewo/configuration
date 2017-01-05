601d66f20db5be795a8362d4817fc3162bc66951
lewo <lewo@abesis.fr>
2017-01-05 21:34:21 +0100

    Fix

--- configuration.nix
+++ configuration.nix
@@ -11,17 +11,21 @@
 
       system.autoUpgrade = {
         enable = true;
-	channel = "https://nixos.org/channels/nixos-unstable";
+	channel = "/var/nixpkgs";
 	extraPath = [ pkgs.git ];
 	preRebuild =
 	''
+	mkdir -p /var/nixpkgs/
+	git -C /var/nixpkgs init
+        git -C /var/nixpkgs remote add origin https://github.com/nlewo/nixpkgs.git || true
+	git -C /var/nixpkgs fetch
+        git -C /var/nixpkgs reset --hard origin/arn
+
         mkdir -p /etc/nixos/
-	pushd /etc/nixos/
-	git init
-        git remote add origin https://github.com/nlewo/configuration.git || true
-	git fetch
-        git reset --hard origin/master
-	popd
+	git -C /etc/nixos init
+        git -C /etc/nixos remote add origin https://github.com/nlewo/configuration.git || true
+	git -C /etc/nixos fetch
+        git -C /etc/nixos reset --hard origin/master
 	'';
 	dates = null;
 	period = "1m";
