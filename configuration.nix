8bf45f3806b5b2d778f9d569f6804b5c646f94a7
lewo <lewo@abesis.fr>
2017-02-13 22:37:06 +0100

    Initial configuration

---
+++ configuration.nix
@@ -0,0 +1,33 @@
+{ config, lib, pkgs, ... }:
+
+let
+  gitPull =
+    ''
+    mkdir -p /var/nixpkgs/
+    git -C /var/nixpkgs init
+    git -C /var/nixpkgs remote add origin https://github.com/nlewo/nixpkgs.git || true
+    git -C /var/nixpkgs pull origin arn
+
+    mkdir -p /etc/nixos/
+    git -C /etc/nixos init
+    git -C /etc/nixos remote add origin https://github.com/nlewo/configuration.git || true
+    git -C /etc/nixos pull origin arn
+    ''; 
+in
+{
+  imports = [ <nixpkgs/nixos/modules/installer/cd-dvd/channel.nix> 
+              ./hardware-configuration.nix
+	      ./grub-configuration.nix
+	    ];
+
+      networking.firewall.allowedTCPPorts = [ 19531 ];
+      services.journald.enableHttpGateway = true;
+
+      system.autoUpgrade = {
+        enable = true;
+	flags = [ "-I" "nixpkgs=/var/nixpkgs" ];
+	extraPath = [ pkgs.git ];
+	preRebuild = gitPull;
+	dates = "*:0/2";
+	};
+}
