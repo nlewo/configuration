ed72f651dca89c3318f1e3f420a57e45af0872e6
lewo <lewo@abesis.fr>
2017-02-18 19:05:23 +0100

    ln -sf configuration.nix

--- configuration/configuration.nix
+++ configuration/configuration.nix
@@ -12,7 +12,7 @@ let
     git -C /var/nixos init
     git -C /var/nixos remote add origin https://github.com/nlewo/configuration.git || true
     git -C /var/nixos pull origin arn
-    ln -s /var/nixos/configuration/configuration.nix /etc/nixos/configuration.nix
+    ln -sf /var/nixos/configuration/configuration.nix /etc/nixos/configuration.nix
     '';
 in
 {
